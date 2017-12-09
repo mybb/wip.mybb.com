124c7927875a1cf88460405288226faa0c9649ec
Justin Soltesz <justin@mybb.com>
2017-12-08 23:23:44 -0500

    Functioning mobile menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,16 @@ $(function() {
         placement: "s",
         mouseOnToPopup: true
     })
+
+    $(".menu-button").click(function () {
+        $(".main-menu").removeClass("main-menu--mobile-hide").addClass("main-menu--mobile-show");
+        $(".menu-button").addClass("menu-button--hide");
+        $(".menu-close-button").removeClass("menu-close-button--hide");
+    });
+
+    $(".menu-close-button").click(function () {
+        $(".main-menu").removeClass("main-menu--mobile-show").addClass("main-menu--mobile-hide");
+        $(".menu-button").removeClass("menu-button--hide");
+        $(".menu-close-button").addClass("menu-close-button--hide");
+    });
 });
